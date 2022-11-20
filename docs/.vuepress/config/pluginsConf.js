2611a354a914c28dc21cd3846309be657038e618
yhh <3378878770@qq.com>
2022-11-20 13:44:27 +0800

    chore: 恢复 secret 文件

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -5,36 +5,36 @@ moment.locale("zh-cn");
 
 
 module.exports = {
-        '@vuepress/last-updated': {
-            transformer: (timestamp, lang) => {
-                return moment(timestamp).format("LLLL")
-            }
-        },
-
-        '@vuepress/pwa': {
-            serviceWorker: true,
-            updatePopup: {
-                message: "发现新内容可用",
-                buttonText: "刷新"
-            }
-        },
-
-        '@vssue/vuepress-plugin-vssue': {
-            // 设置 `platform` 而不是 `api`
-            platform: 'github-v4',
-
-            // 其他的 Vssue 配置
-            owner: 'yhh-j',
-            repo: 'docs',
-            clientId: secret.clientId,
-            clientSecret: secret.clientSecret,
-            // autoCreateIssue: true,
-        },
-
-        '@vuepress/back-to-top': true,
-
-        '@vuepress/google-analytics': {
-
-            'ga': secret.ga // UA-00000000-0
+    '@vuepress/last-updated': {
+        transformer: (timestamp, lang) => {
+            return moment(timestamp).format("LLLL")
         }
+    },
+
+    '@vuepress/pwa': {
+        serviceWorker: true,
+        updatePopup: {
+            message: "发现新内容可用",
+            buttonText: "刷新"
+        }
+    },
+
+    '@vssue/vuepress-plugin-vssue': {
+        // 设置 `platform` 而不是 `api`
+        platform: 'github-v4',
+
+        // 其他的 Vssue 配置
+        owner: 'yhh-j',
+        repo: 'docs',
+        clientId: secret.clientId,
+        clientSecret: secret.clientSecret,
+        // autoCreateIssue: true,
+    },
+
+    '@vuepress/back-to-top': true,
+
+    '@vuepress/google-analytics': {
+
+        'ga': secret.ga // UA-00000000-0
+    }
 }
---
+++ docs/.vuepress/config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clientId: '22eb974a5b2060987158',
+    clientSecret: 'fe4e9e28aa6c6af438a05383a7be8e01be22e59e',
+    'ga': 'G-N507NZDCS5'
+}
