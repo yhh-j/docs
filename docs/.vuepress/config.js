e5d1561c09bb7e53ac4d183a15faac8ae1e4f3fb
yhh <3378878770@qq.com>
2022-11-26 15:52:00 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,15 +11,6 @@ module.exports = {
     themeConfig: {
         logo: 'hero.jpg',
         lastUpdated: '上次更新时间',
-        docsRepo: 'yhh-j/docs',
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        // 假如文档放在一个特定的分支下：
-        docsBranch: 'master',
-        // 默认是 false, 设置为 true 来启用
-        editLinks: true,
-        // 默认为 "Edit this page"
-        editLinkText: '编辑文档',
         nav: navConf,
         // sidebar: sidebarConf,
     }
